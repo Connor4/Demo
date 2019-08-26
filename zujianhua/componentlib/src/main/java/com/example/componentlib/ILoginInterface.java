0aeee4e3b25797bda6156310732130ec99761297
Connor <acooonnor@163.com>
2019-08-26 22:48:02 +0800

    实现组件部分module library切换

---
+++ zujianhua/componentlib/src/main/java/com/example/componentlib/ILoginInterface.java
@@ -0,0 +1,8 @@
+package com.example.componentlib;
+
+import android.content.Context;
+
+public interface ILoginInterface {
+
+    void launchActivity(Context context);
+}
---
+++ zujianhua/componentlib/src/main/java/com/example/componentlib/IMineInterface.java
@@ -0,0 +1,7 @@
+package com.example.componentlib;
+
+import android.content.Context;
+
+public interface IMineInterface {
+    void launchActivity(Context context);
+}
