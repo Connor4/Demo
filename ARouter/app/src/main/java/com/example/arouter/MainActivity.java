f1f520d408cd8033053853b60472d635e347ef50
Connor <acooonnor@163.com>
2019-08-27 10:23:57 +0800

    提交代码

--- ARouter/app/src/main/java/com/example/arouter/MainActivity.java
+++ ARouter/app/src/main/java/com/example/arouter/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.arouter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        findViewById(R.id.member).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
 }
