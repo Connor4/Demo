fee60d8255b889f5d9e18e18cc9b1f1a036c86f3
dzb <dengzhibin03@countrygarden.com.cn>
2020-06-18 17:01:28 +0800

    提交用例

--- Demo/app/src/main/java/com/connor/demo/RxJava/RxJavaActivity.java
+++ Demo/app/src/main/java/com/connor/demo/RxJava/RxJavaActivity.java
@@ -1,6 +1,7 @@
 package com.connor.demo.RxJava;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,12 +10,15 @@ import androidx.annotation.Nullable;
 
 import com.connor.demo.R;
 
+import java.io.File;
+
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
-import io.reactivex.disposables.Disposable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class RxJavaActivity extends Activity {
     private static final String TAG = "dzb";
@@ -42,87 +46,87 @@ public class RxJavaActivity extends Activity {
                 }
                 e.onComplete();
             }
-        })  .subscribeOn(Schedulers.io()) // 订阅者线程
-            .observeOn(AndroidSchedulers.mainThread()) // 观察者线程
-            .subscribe(new Observer<Integer>() {
-            private Disposable mDisposable;
+        }).subscribeOn(Schedulers.io()) // 订阅者线程
+                .observeOn(AndroidSchedulers.mainThread()) // 观察者线程
+                .subscribe(new Observer<Integer>() {
+                    private Disposable mDisposable;
 
-            @Override
-            public void onSubscribe(Disposable d) {
-                mDisposable = d;
-            }
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        mDisposable = d;
+                    }
 
-            @Override
-            public void onNext(Integer integer) {
-                if (integer > 2) {
-                    mDisposable.dispose();
-                }
-                Log.d(TAG, "onNext  " + integer);
-            }
+                    @Override
+                    public void onNext(Integer integer) {
+                        if (integer > 2) {
+                            mDisposable.dispose();
+                        }
+                        Log.d(TAG, "onNext  " + integer);
+                    }
 
-            @Override
-            public void onError(Throwable e) {
+                    @Override
+                    public void onError(Throwable e) {
 
-            }
+                    }
 
-            @Override
-            public void onComplete() {
-                Log.d(TAG, "onComplete  ");
-            }
-        });
+                    @Override
+                    public void onComplete() {
+                        Log.d(TAG, "onComplete  ");
+                    }
+                });
     }
-    
- private void loadImage(){
- new Thread() {
-    @Override
-    public void run() {
-        super.run();
-        for (File folder : folders) {
-            File[] files = folder.listFiles();
-            for (File file : files) {
-                if (file.getName().endsWith(".png")) {
-                    final Bitmap bitmap = getBitmapFromFile(file);
-                    getActivity().runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            imageCollectorView.addImage(bitmap);
-                            }
-                        });
+
+    private void loadImage() {
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                for (File folder : folders) {
+                    File[] files = folder.listFiles();
+                    for (File file : files) {
+                        if (file.getName().endsWith(".png")) {
+                            final Bitmap bitmap = getBitmapFromFile(file);
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    imageCollectorView.addImage(bitmap);
+                                }
+                            });
+                        }
                     }
                 }
             }
-        }
-    }.start();
-}
+        }.start();
+    }
 
-private void rxLoadImage(){
-    Observable.from(folders)
-    .flatMap(new Func1<File, Observable<File>>() {
-        @Override
-        public Observable<File> call(File file) {
-            return Observable.from(file.listFiles());
-        }
-    })
-    .filter(new Func1<File, Boolean>() {
-        @Override
-        public Boolean call(File file) {
-            return file.getName().endsWith(".png");
-        }
-    })
-    .map(new Func1<File, Bitmap>() {
-        @Override
-        public Bitmap call(File file) {
-            return getBitmapFromFile(file);
-        }
-    })
-    .subscribeOn(Schedulers.io())
-    .observeOn(AndroidSchedulers.mainThread())
-    .subscribe(new Action1<Bitmap>() {
-        @Override
-        public void call(Bitmap bitmap) {
-            imageCollectorView.addImage(bitmap);
-        }
-    });
-}
+    private void rxLoadImage() {
+        Observable.from(folders)
+                .flatMap(new Func1<File, Observable<File>>() {
+                    @Override
+                    public Observable<File> call(File file) {
+                        return Observable.from(file.listFiles());
+                    }
+                })
+                .filter(new Func1<File, Boolean>() {
+                    @Override
+                    public Boolean call(File file) {
+                        return file.getName().endsWith(".png");
+                    }
+                })
+                .map(new Func1<File, Bitmap>() {
+                    @Override
+                    public Bitmap call(File file) {
+                        return getBitmapFromFile(file);
+                    }
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<Bitmap>() {
+                    @Override
+                    public void call(Bitmap bitmap) {
+                        imageCollectorView.addImage(bitmap);
+                    }
+                });
+    }
 
 }
