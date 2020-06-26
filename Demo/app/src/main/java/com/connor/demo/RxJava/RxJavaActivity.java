ab14d030602660d9cdc2d9943fd9e68df0228226
dzb <acooonnor@163.com>
2020-06-26 20:23:31 +0800

    修改androidx使用

--- Demo/app/src/main/java/com/connor/demo/RxJava/RxJavaActivity.java
+++ Demo/app/src/main/java/com/connor/demo/RxJava/RxJavaActivity.java
@@ -13,8 +13,9 @@ import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
-import io.reactivex.disposables.Disposable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class RxJavaActivity extends Activity {
     private static final String TAG = "dzb";
