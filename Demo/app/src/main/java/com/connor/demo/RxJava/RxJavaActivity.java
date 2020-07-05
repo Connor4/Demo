daed277c589d00cbf15354e4d8f7a54adbcd259e
dzb <acooonnor@163.com>
2020-07-05 10:53:38 +0800

    注释部分代码隐藏警告

--- Demo/app/src/main/java/com/connor/demo/RxJava/RxJavaActivity.java
+++ Demo/app/src/main/java/com/connor/demo/RxJava/RxJavaActivity.java
@@ -1,7 +1,6 @@
 package com.connor.demo.RxJava;
 
 import android.app.Activity;
-import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,8 +8,6 @@ import android.view.View;
 import androidx.annotation.Nullable;
 
 import com.connor.demo.R;
-
-import java.io.File;
 
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
@@ -76,57 +73,57 @@ public class RxJavaActivity extends Activity {
                 });
     }
 
-    private void loadImage() {
-        new Thread() {
-            @Override
-            public void run() {
-                super.run();
-                for (File folder : folders) {
-                    File[] files = folder.listFiles();
-                    for (File file : files) {
-                        if (file.getName().endsWith(".png")) {
-                            final Bitmap bitmap = getBitmapFromFile(file);
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    imageCollectorView.addImage(bitmap);
-                                }
-                            });
-                        }
-                    }
-                }
-            }
-        }.start();
-    }
-
-    private void rxLoadImage() {
-        Observable.from(folders)
-                .flatMap(new Func1<File, Observable<File>>() {
-                    @Override
-                    public Observable<File> call(File file) {
-                        return Observable.from(file.listFiles());
-                    }
-                })
-                .filter(new Func1<File, Boolean>() {
-                    @Override
-                    public Boolean call(File file) {
-                        return file.getName().endsWith(".png");
-                    }
-                })
-                .map(new Func1<File, Bitmap>() {
-                    @Override
-                    public Bitmap call(File file) {
-                        return getBitmapFromFile(file);
-                    }
-                })
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Action1<Bitmap>() {
-                    @Override
-                    public void call(Bitmap bitmap) {
-                        imageCollectorView.addImage(bitmap);
-                    }
-                });
-    }
+//    private void loadImage() {
+//        new Thread() {
+//            @Override
+//            public void run() {
+//                super.run();
+//                for (File folder : folders) {
+//                    File[] files = folder.listFiles();
+//                    for (File file : files) {
+//                        if (file.getName().endsWith(".png")) {
+//                            final Bitmap bitmap = getBitmapFromFile(file);
+//                            runOnUiThread(new Runnable() {
+//                                @Override
+//                                public void run() {
+//                                    imageCollectorView.addImage(bitmap);
+//                                }
+//                            });
+//                        }
+//                    }
+//                }
+//            }
+//        }.start();
+//    }
+//
+//    private void rxLoadImage() {
+//        Observable.from(folders)
+//                .flatMap(new Func1<File, Observable<File>>() {
+//                    @Override
+//                    public Observable<File> call(File file) {
+//                        return Observable.from(file.listFiles());
+//                    }
+//                })
+//                .filter(new Func1<File, Boolean>() {
+//                    @Override
+//                    public Boolean call(File file) {
+//                        return file.getName().endsWith(".png");
+//                    }
+//                })
+//                .map(new Func1<File, Bitmap>() {
+//                    @Override
+//                    public Bitmap call(File file) {
+//                        return getBitmapFromFile(file);
+//                    }
+//                })
+//                .subscribeOn(Schedulers.io())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe(new Action1<Bitmap>() {
+//                    @Override
+//                    public void call(Bitmap bitmap) {
+//                        imageCollectorView.addImage(bitmap);
+//                    }
+//                });
+//    }
 
 }
