01a1fa179fc3a32d1e87d6fca8f70416bdbf5983
dzb <dengzhibin03@countrygarden.com.cn>
2020-07-10 12:33:38 +0800

    添加用例

--- Demo/app/src/main/java/com/connor/demo/designpattern/Proxy.java
+++ Demo/app/src/main/java/com/connor/demo/designpattern/Proxy.java
@@ -5,32 +5,79 @@ package com.connor.demo.designpattern;
  */
 public class Proxy {
 
-    abstract class Subject {
-        abstract void request();
-    }
-
-    class RealSubject extends Subject {
-        @Override
-        void request() {
-            System.out.println("真实的请求");
-        }
-    }
-
-    class ProxyClass extends Subject {
-        RealSubject realSubject;
-
-        @Override
-        void request() {
-            if (realSubject == null) {
-                realSubject = new RealSubject();
-            }
-            realSubject.request();
-        }
-    }
-
-    public void test() {
+    public static void main(String[] args) {
         ProxyClass proxyClass = new ProxyClass();
         proxyClass.request();
+
+        // 具体用例
+        Image image = new ProxyImage("test_10mb.jpg");
+        // 图像将从磁盘加载
+        image.display();
+        // 图像不需要从磁盘加载
+        image.display();
     }
 
+}
+
+abstract class Subject {
+    abstract void request();
+}
+
+class RealSubject extends Subject {
+    @Override
+    void request() {
+        System.out.println("真实的请求");
+    }
+}
+
+class ProxyClass extends Subject {
+    RealSubject realSubject;
+
+    @Override
+    void request() {
+        if (realSubject == null) {
+            realSubject = new RealSubject();
+        }
+        realSubject.request();
+    }
+}
+
+// 具体用例
+interface Image {
+    void display();
+}
+
+class RealImage implements Image {
+    private String mFileName;
+
+    public RealImage(String fileName) {
+        this.mFileName = fileName;
+        loadFromDisk();
+    }
+
+    private void loadFromDisk() {
+        System.out.println("load image from  disk");
+    }
+
+    @Override
+    public void display() {
+        System.out.println("display image");
+    }
+}
+
+class ProxyImage implements Image {
+    private RealImage mRealImage;
+    private String mFileName;
+
+    public ProxyImage(String mFileName) {
+        this.mFileName = mFileName;
+    }
+
+    @Override
+    public void display() {
+        if (mRealImage == null) {
+            mRealImage = new RealImage(mFileName);
+        }
+        mRealImage.display();
+    }
 }
