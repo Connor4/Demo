1a136ca3048c6c9f993c6abaea1361f68a80e9d5
dzb <acooonnor@163.com>
2020-07-13 23:10:22 +0800

    责任链模式

--- Demo/app/src/main/java/com/connor/demo/designpattern/Chain.java
+++ Demo/app/src/main/java/com/connor/demo/designpattern/Chain.java
@@ -5,55 +5,7 @@ package com.connor.demo.designpattern;
  */
 public class Chain {
 
-    // 抽象处理基类
-    abstract class Handler {
-        protected String name;
-        protected Handler successor;
-
-        public Handler(String name) {
-            this.name = name;
-        }
-
-        public void setSuccessor(Handler superior) {
-            this.successor = superior;
-        }
-
-        abstract void handleWork(int request);
-    }
-
-    // 具体实现类1
-    class ConcreteHandler1 extends Handler {
-        public ConcreteHandler1(String name) {
-            super(name);
-        }
-
-        @Override
-        void handleWork(int request) {
-            if (request >= 0 && request < 10) {
-                System.out.println("ConcreteHandler1 handleWork " + request);
-            } else {
-                successor.handleWork(request);
-            }
-        }
-    }
-
-    // 具体实现类2
-    class ConcreteHandler2 extends Handler {
-        public ConcreteHandler2(String name) {
-            super(name);
-        }
-
-        @Override
-        void handleWork(int request) {
-            if (request >= 10 && request < 20) {
-                System.out.println("ConcreteHandler2 handleWork " + request);
-            } else {
-                successor.handleWork(request);
-            }
-        }
-    }
-
-    public void test() {
+    public static void main(String[] args) {
         Handler handler1 = new ConcreteHandler1("ConcreteHandler1");
         Handler handler2 = new ConcreteHandler2("ConcreteHandler2");
         handler1.setSuccessor(handler2);
@@ -64,4 +16,52 @@ public class Chain {
         }
     }
 
+}
+
+// 抽象处理基类
+abstract class Handler {
+    protected String name;
+    protected Handler successor;
+
+    public Handler(String name) {
+        this.name = name;
+    }
+
+    public void setSuccessor(Handler superior) {
+        this.successor = superior;
+    }
+
+    abstract void handleWork(int request);
+}
+
+// 具体实现类1
+class ConcreteHandler1 extends Handler {
+    public ConcreteHandler1(String name) {
+        super(name);
+    }
+
+    @Override
+    void handleWork(int request) {
+        if (request >= 0 && request < 10) {
+            System.out.println("ConcreteHandler1 handleWork " + request);
+        } else {
+            successor.handleWork(request);
+        }
+    }
+}
+
+// 具体实现类2
+class ConcreteHandler2 extends Handler {
+    public ConcreteHandler2(String name) {
+        super(name);
+    }
+
+    @Override
+    void handleWork(int request) {
+        if (request >= 10 && request < 20) {
+            System.out.println("ConcreteHandler2 handleWork " + request);
+        } else {
+            successor.handleWork(request);
+        }
+    }
 }
