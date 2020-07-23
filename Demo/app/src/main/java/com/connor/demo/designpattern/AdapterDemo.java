bf296e1e29d66a6036e63f052efb1266e8d4c835
dzb <dengzhibin03@countrygarden.com.cn>
2020-07-23 15:54:07 +0800

    适配器模式添加注释

--- Demo/app/src/main/java/com/connor/demo/designpattern/AdapterDemo.java
+++ Demo/app/src/main/java/com/connor/demo/designpattern/AdapterDemo.java
@@ -2,6 +2,10 @@ package com.connor.demo.designpattern;
 
 /**
  * 适配器模式
+ *将一个类的接口转换成客户希望的另外一个接口，adapter模式是的本来由于接口不兼容而不能一起工作的类可以一起工作
+ *
+ * 使用场景：
+ *两个类需要做的事相同或者相似，但是具有不同的接口要使用它。
  */
 public class AdapterDemo {
     public static void main(String[] args) {
